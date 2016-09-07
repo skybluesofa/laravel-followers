d2831897e7b5341409693adbeb7b6236e035c9ce
skybluesofa <connect@skybluesofa.com>
2016-09-07 06:20:25 -0500

    Travis Updates

--- tests/config/database.php
+++ tests/config/database.php
@@ -4,10 +4,10 @@ return array(
     'connections' => array(
         'testing' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'follower_test'),
-            'username'  => env('DB_TEST_USERNAME', 'travis'),
-            'password'  => env('DB_TEST_PASSWORD', ''),
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'follower_test'),
+            'username'  => env('DB_USERNAME', 'travis'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
